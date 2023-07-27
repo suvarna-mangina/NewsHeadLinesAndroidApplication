5601e4c53a7a8c1df9e4c86f2b0261c07ca99ac2
suvarna-mangina <110011467+suvarna-mangina@users.noreply.github.com>
2023-07-27 21:26:22 +0530

    Create MainPage.kt

---
+++ MainPage.kt
@@ -0,0 +1,182 @@
+package com.example.androidnews
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+import coil.compose.rememberImagePainter
+import coil.size.Scale
+import coil.transform.CircleCropTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.androidnews.ui.theme.AndroidnewsTheme
+
+class MainPage : ComponentActivity() {
+    val mainViewModel by viewModels<MainViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidnewsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Column() {
+
+
+                        Text(text = "Latest NEWS", fontSize = 32.sp, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+
+                        MovieList(applicationContext, movieList = mainViewModel.movieListResponse)
+                        mainViewModel.getMovieList()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieList(context: Context, movieList: List<Articles>) {
+    var selectedIndex by remember { mutableStateOf(-1) }
+    LazyColumn {
+
+        itemsIndexed(items = movieList) {
+                index, item ->
+            MovieItem(context,movie = item, index, selectedIndex) { i ->
+                selectedIndex = i
+            }
+        }
+    }
+
+}
+
+@Composable
+fun MovieItem(context: Context) {
+    val movie = Articles(
+        "Coco",
+        "",
+        " articl"
+    )
+
+
+    MovieItem(context,movie = movie, 0, 0) { i ->
+        Log.i("wertytest123abc", "MovieItem: "
+                +i)
+    }
+}
+
+@Composable
+fun MovieItem(context: Context, movie: Articles, index: Int, selectedIndex: Int,
+              onClick: (Int) -> Unit)
+{
+
+    val backgroundColor = if (index == selectedIndex) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.background
+
+    Card(
+        modifier = Modifier
+            .padding(8.dp, 4.dp)
+            .fillMaxSize()
+            .selectable(true, true, null,
+                onClick = {
+                    Log.i("test123abc", "MovieItem: $index/n$selectedIndex")
+                })
+            .clickable { onClick(index) }
+            .height(180.dp), shape = RoundedCornerShape(8.dp), elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Surface(color = Color.White) {
+
+            Row(
+                Modifier
+                    .padding(4.dp)
+                    .fillMaxSize()
+
+            )
+            {
+                Image(
+                    painter = rememberImagePainter(
+                        data = movie.urlToImage,
+                        builder = {
+                            scale(Scale.FILL)
+                            placeholder(R.drawable.placeholder)
+                            transformations(CircleCropTransformation())
+                        }
+                    ),
+                    contentDescription = movie.description,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(0.3f)
+                )
+
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxHeight()
+                        .weight(0.8f)
+                        .background(Color.Gray)
+                        .padding(20.dp)
+                        .selectable(true, true, null,
+                            onClick = {
+                                Log.i("test123abc", "MovieItem: $index/n${movie.description}")
+                                context.startActivity(
+                                    Intent(context, DisplayNews::class.java)
+                                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                        .putExtra("desk", movie.description.toString())
+                                        .putExtra("urlToImage", movie.urlToImage)
+                                        .putExtra("title", movie.title)
+                                )
+                            })
+                ) {
+
+                    Text(
+                        text = movie.title.toString(),
+                        style = MaterialTheme.typography.labelLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    HtmlText(html = movie.description.toString())
+                }
+            }
+        }
+    }
+    @Composable
+    fun HtmlText(html: String, modifier: Modifier = Modifier) {
+        AndroidView(
+            modifier = modifier
+                .fillMaxSize()
+                .size(33.dp),
+            factory = { context -> TextView(context) },
+            update = { it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT) }
+        )
+    }
+}
