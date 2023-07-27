a2a50d28238159ed944e94e763dc2725167da0d9
suvarna-mangina <110011467+suvarna-mangina@users.noreply.github.com>
2023-07-27 21:27:00 +0530

    Create DisplayNews.kt

---
+++ DisplayNews.kt
@@ -0,0 +1,80 @@
+package com.example.androidnews
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+import coil.compose.rememberImagePainter
+import com.example.androidnews.ui.theme.AndroidnewsTheme
+
+
+class DisplayNews : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidnewsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(  modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    var desk = getIntent().getStringExtra("desk")
+                    var title = getIntent().getStringExtra("title")
+                    var uriImage = getIntent().getStringExtra("urlToImage")
+                    Log.i("test123abc", "MovieItem: $desk")
+
+                    Column(Modifier.background(Color.Gray).padding(20.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                        Text(text = ""+title, fontSize = 32.sp)
+                        HtmlText(html = desk.toString())
+                        /*  AsyncImage(
+                              model = "https://example.com/image.jpg",
+                              contentDescription = "Translated description of what the image contains"
+                          )*/
+                        Image(
+                            painter = rememberImagePainter(uriImage),
+                            contentDescription = "My content description",
+                        )
+                    }
+                    //   Greeting(desk.toString())
+                }
+            }
+        }
+    }
+}
+@Composable
+fun Greeting(name: String) {
+    // Text(text = "Hello $name!")
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndroidnewsTheme {
+        //   Greeting("Android")
+    }
+}
+@Composable
+fun HtmlText(html: String, modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context -> TextView(context) },
+        update = { it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT) }
+    )
+}
